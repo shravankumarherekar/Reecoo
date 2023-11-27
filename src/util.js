593c5f10a17220e299fd0b39deadf7d92156d83b
shravankumarherkar <shravan.herekar01@gmail.com>
2023-11-27 20:18:57 +0530

    intial commit

---
+++ src/util.js
@@ -0,0 +1,133 @@
+export const PRODUCTS_INVENTORY_JSON = [
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmany",
+      price: 28.1,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:1
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:2
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivids",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:3
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:4
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:5
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:6
+    },{
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black LabelmanyAW Diary",
+      price: 28.1,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:7
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invividt",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 121.34,
+      status: "none",
+      id:8
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmanyy",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:9
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:10
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:11
+    },
+    {
+      product_image:
+        "https://res.cloudinary.com/dybkmn7is/image/upload/v1698585328/Avocado_Hass_ffs1mc.jpg",
+      product_name: "Chicken Breast Fillets, Boneless matuu Marinated 6 ounce Raw, Invivid",
+      brand: "Hormel Black Labelmany",
+      price: 60.67,
+      quantity: 3,
+      total: 84.3,
+      status: "none",
+      id:12
+    }];
+  
